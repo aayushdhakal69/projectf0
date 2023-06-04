71f1bc1046abdf60a46cca688c2ae0a030798fed
aayushdhakal69 <therespawner69@gmail.com>
2023-06-04 19:05:21 +0545

    contact,about,services,photos

--- myapp/views.py
+++ myapp/views.py
@@ -10,3 +10,9 @@ def index(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+def services(request):
+    return render(request, "services.html")
